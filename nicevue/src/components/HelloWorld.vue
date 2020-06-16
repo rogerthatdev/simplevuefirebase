60ef66badb9d0f4649f0cac37c77cb6b743e6232
Roger Martinez <rgmartinez@google.com>
2020-06-16 10:40:07 -0700

    HelloWorld template changes

--- nicevue/src/components/HelloWorld.vue
+++ nicevue/src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="hello">
+  <button v-if="!authenticated" @click="login">Login</button>
+  <div v-if="authenticated">
+  <button @click="logout">Logout</button>
+  <h1>Hi {{ firstName }}!</h1>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -27,6 +31,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    </div>
   </div>
 </template>
 
